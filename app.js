3b3b4098fb5387696adb170c1f979d38036dffc3
IvanRave <reyvart@hotmail.com>
2013-10-11 21:56:01 +0300

    Api reviews init

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/api/reviews', function(req, res) {
+    res.send('This is not implemented now');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
